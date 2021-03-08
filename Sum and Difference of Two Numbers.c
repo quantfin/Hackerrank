8dbae28154a767ae6457cbb6512457ff85e6297e
Akash Ranjan <akash.hallo@gmail.com>
2021-03-08 19:53:03 +0530

    Add files via upload

---
+++ Sum and Difference of Two Numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main()
+{
+    int a , b;
+    float c, d;
+
+    scanf("%d %d",&a,&b);
+    printf("%d %d" , a+b , a-b);
+
+    scanf("%f %f",&c ,&d);
+    printf("%f %f",c+d ,c-d);
+
+
+    return 0;
+
+}
+
