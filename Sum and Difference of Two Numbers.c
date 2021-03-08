9fd4e73874f169fdff542d7c49e6bb4fb8e91928
Akash Ranjan <akash.hallo@gmail.com>
2021-03-08 20:13:08 +0530

    Update Sum and Difference of Two Numbers.c

--- Sum and Difference of Two Numbers.c
+++ Sum and Difference of Two Numbers.c
@@ -8,14 +8,14 @@
 
 int main()
 {
-    int a , b;
-    float c, d;
+    int a , b;      //declare a and b integer.
+    float c, d;     //declare c and d float.
+  
+    scanf("%d %d",&a,&b); // for reading user input for value of a and b.
+    printf("%d %d\n", a+b , a-b); // value of sum and substraction will be calculaed as well as printed.
 
-    scanf("%d %d",&a,&b);
-    printf("%d %d\n", a+b , a-b);
-
-    scanf("%f %f",&c ,&d);
-    printf("%f %f",c+d ,c-d);
+    scanf("%f %f",&c ,&d); // read c and d value from user
+    printf("%0.1f %0.1f",c+d ,c-d); // we use 0.1 before f to show only one digit after decimal point and print the computed value as ans.
 
 
     return 0;
